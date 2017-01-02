9be5f08dc0c70593688b4f297fd6cde9a24704fe
Michael Sørensen <michaelsorensen@Michaels-MacBook-Pro.local>
2017-01-02 15:21:49 +0100

    Author check correction

--- bbpress-best-answer.php
+++ bbpress-best-answer.php
@@ -128,7 +128,7 @@ class Plugin
 
     public function show_tool()
     {
-        if(current_user_can('manage_options') || bbp_get_topic_author_id(get_the_ID()) === get_current_user_id())
+        if(current_user_can('manage_options') || bbp_get_topic_author_id(bbp_get_reply_topic_id(get_the_ID())) === get_current_user_id())
         {
             return true;
         }
