809d6fe74b10c62835e689b6f9016088731cfa9d
Michael Sørensen <michael.sorensen@bonnier.dk>
2017-01-03 11:03:22 +0100

    fix typo

--- src/Bonnier/WP/BestAnswer/Forum/Reply.php
+++ src/Bonnier/WP/BestAnswer/Forum/Reply.php
@@ -113,7 +113,7 @@ class Reply
     public static function has_access($replyId)
     {
         if(current_user_can('manage_options')
-            || bbp_get_topic_author_id(bbp_get_reply_topic_id($replId))  === get_current_user_id()) {
+            || bbp_get_topic_author_id(bbp_get_reply_topic_id($replyId))  === get_current_user_id()) {
             return true;
         }
 
