ca702da38344bb1428593f33073e7b2aed53069c
Michael Sørensen <michael.sorensen@bonnier.dk>
2017-01-20 10:17:52 +0100

    updated version

--- src/Bonnier/WP/BestAnswer/Admin/BulkAction.php
+++ src/Bonnier/WP/BestAnswer/Admin/BulkAction.php
@@ -27,7 +27,7 @@ class BulkAction
             add_filter('bulk_actions-edit-'.bbp_get_topic_post_type(), [__CLASS__, 'my_custom_bulk_actions']);
             add_filter('handle_bulk_actions-edit-'.bbp_get_topic_post_type(), [__CLASS__, 'my_bulk_action_handler'], 10, 3);
             add_action('admin_notices', [__CLASS__, 'my_bulk_action_admin_notice']);
-            
+
         }
     }
 
